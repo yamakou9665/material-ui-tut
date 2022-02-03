e80a8bbdbcb67c2e43ec5510970840db99c8d41d
Kohei_Yamada <32315737+yamakou9665@users.noreply.github.com>
2022-02-03 15:47:00 +0900

    #6 まで終了

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { ThemeProvider, createMuiTheme } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe"
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: "quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+
+const useStyles = makeStyles({});
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        gutterBottom
+        component="h2"
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        onClick={() => console.log("Yout clicked me!")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
